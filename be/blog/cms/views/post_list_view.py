0f7fc2c5c40c8f79758b38af65ac48c7e1c81eeb
Mojtaba Michael <mojimich2015@gmail.com>
2024-03-04 14:04:47 +0300

    Added Pagination For Posts in cms app

--- be/blog/cms/views/post_list_view.py
+++ be/blog/cms/views/post_list_view.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from cms.models import Post
+from django.core.paginator import Paginator
 
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
     return render(
         request,
         'cms/post/list.html',
