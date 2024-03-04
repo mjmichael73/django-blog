78f7fa06b783de3d8bf03b5b1f1879f5071a9abc
Mojtaba Michael <mojimich2015@gmail.com>
2024-03-04 14:10:12 +0300

    Handled not integer page numbers in post_list view in cms app post_list view

--- be/blog/cms/views/post_list_view.py
+++ be/blog/cms/views/post_list_view.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from cms.models import Post
-from django.core.paginator import Paginator, EmptyPage
+from django.core.paginator import (
+    Paginator,
+    EmptyPage,
+    PageNotAnInteger
+)
 
 
 def post_list(request):
@@ -9,6 +13,8 @@ def post_list(request):
     page_number = request.GET.get('page', 1)
     try:
         posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
     return render(
