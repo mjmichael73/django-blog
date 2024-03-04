8013da6ec219c54806664f09a2e95829c19830d7
Mojtaba Michael <mojimich2015@gmail.com>
2024-03-04 14:08:03 +0300

    Handled pagination errors in post_list view

--- be/blog/cms/views/post_list_view.py
+++ be/blog/cms/views/post_list_view.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from cms.models import Post
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage
 
 
 def post_list(request):
     post_list = Post.published.all()
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(
         request,
         'cms/post/list.html',
