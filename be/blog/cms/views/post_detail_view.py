7b9d01e52175609002676bbd3bbaaa879b5eae47
Mojtaba Michael <mojimich2015@gmail.com>
2024-03-06 13:50:19 +0300

    Added similar posts fuctionality

--- be/blog/cms/views/post_detail_view.py
+++ be/blog/cms/views/post_detail_view.py
@@ -2,6 +2,7 @@ from django.http import Http404
 from cms.models import Post, Comment
 from django.shortcuts import render, get_object_or_404
 from cms.forms import CommentForm
+from django.db.models import Count
 
 
 def post_detail(request, year, month, day, post):
@@ -15,12 +16,22 @@ def post_detail(request, year, month, day, post):
     )
     comments = post.comments.filter(active=True)
     form = CommentForm()
+
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(
+        tags__in=post_tags_ids
+    ).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(
+        same_tags=Count('tags')
+    ).order_by('-same_tags', '-publish')[:4]
     return render(
         request,
         'cms/post/detail.html',
         {
             'post': post,
             'comments': comments,
-            'form': form
+            'form': form,
+            'similar_posts': similar_posts
         }
     )
