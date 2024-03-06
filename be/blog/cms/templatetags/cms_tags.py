06c69d28acf29a2e5c7f5a3bdc56ccc7dd80284b
Mojtaba Michael <mojimich2015@gmail.com>
2024-03-06 13:59:13 +0300

    Added simple_tag and inclusion tags for sidebar

---
+++ be/blog/cms/templatetags/cms_tags.py
@@ -0,0 +1,15 @@
+from django import template
+from cms.models import Post
+
+register = template.Library()
+
+
+@register.simple_tag
+def total_posts():
+    return Post.published.count()
+
+
+@register.inclusion_tag("cms/post/latest_posts.html")
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {"latest_posts": latest_posts}
