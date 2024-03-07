f70894a8d673f648b180cbcf5ebb7f306bd7581c
Mojtaba Michael <mojimich2015@gmail.com>
2024-03-07 17:26:45 +0300

    Added SearchRank to the post_search view

--- be/blog/cms/views/post_search_view.py
+++ be/blog/cms/views/post_search_view.py
@@ -1,4 +1,8 @@
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank
+)
 from cms.forms import SearchForm
 from cms.models import Post
 from django.shortcuts import render
@@ -13,9 +17,13 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
+            search_rank = SearchRank(search_vector, search_query)
             results = Post.published.annotate(
-                search=SearchVector('title', 'body'),
-            ).filter(search=query)
+                search=search_vector,
+                rank=search_rank,
+            ).filter(search=search_query).order_by('-rank')
     return render(
         request,
         'cms/post/search.html',
