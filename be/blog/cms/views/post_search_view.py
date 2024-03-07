8f15880417b94d357cfdc2f0539a67b49d9dc5f4
Mojtaba Michael <mojimich2015@gmail.com>
2024-03-07 17:39:01 +0300

    Added weight to the post_search queries

--- be/blog/cms/views/post_search_view.py
+++ be/blog/cms/views/post_search_view.py
@@ -17,15 +17,20 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            search_vector = SearchVector('title', 'body')
+            # search_vector = SearchVector('title', 'body')
+            search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
             # search_vector = SearchVector('title', 'body', config='spanish')
             search_query = SearchQuery(query)
             # search_query = SearchQuery(query, config='spanish')
             search_rank = SearchRank(search_vector, search_query)
+            # results = Post.published.annotate(
+            #     search=search_vector,
+            #     rank=search_rank,
+            # ).filter(search=search_query).order_by('-rank')
             results = Post.published.annotate(
                 search=search_vector,
                 rank=search_rank,
-            ).filter(search=search_query).order_by('-rank')
+            ).filter(rank__gte=0.3).order_by('-rank')
     return render(
         request,
         'cms/post/search.html',
